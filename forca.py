5269f6e9801e11d2cd127133f26f4a5881205b5c
Karla Renata <karlarenata692@gmail.com>
2025-04-30 17:23:46 -0300

    atualização

--- forca.py
+++ forca.py
@@ -4,7 +4,11 @@ def jogar():
     print("Bem vindo ao jogo de Forca")
     print("**************************")
 
-
+    palavra_secreta = "banana"
+    while ()
+        print("jogando...")
+        
+        
     print("Fim do Jogo!")
     
 if (__name__ = "__main__"):
